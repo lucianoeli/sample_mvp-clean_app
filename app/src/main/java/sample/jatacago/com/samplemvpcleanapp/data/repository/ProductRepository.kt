3ffaecd8a44c29be4ca206146becbaf47d846b80
lucho eli <luciano.elissondo@globant.com>
2019-02-20 17:02:24 -0300

    comments

--- app/src/main/java/sample/jatacago/com/samplemvpcleanapp/data/repository/ProductRepository.kt
+++ app/src/main/java/sample/jatacago/com/samplemvpcleanapp/data/repository/ProductRepository.kt
@@ -1,10 +1,12 @@
 package sample.jatacago.com.samplemvpcleanapp.data.repository
 
-import io.reactivex.Single
+import io.reactivex.Single //observable que emite 1 solo valor o error
 import sample.jatacago.com.samplemvpcleanapp.domain.entities.Product
 import sample.jatacago.com.samplemvpcleanapp.domain.repository.ProductRepository as IProductRepository
 
 class ProductRepository : IProductRepository {
+
+    // implementa repositorio, y brinda el metodo para recuperar los datos de una DB o Service Call
     override fun getBySailing(sailingId: String): Single<List<Product>> =
         Single.just(listOf(Product("ASE28328", "Flow Rider", "AL02122019")))
 }
